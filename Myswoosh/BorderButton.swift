a9eb3fb024b17fd60dcb70d603f258cc4750912b
Abdelkoddous <dslpo@gmx.de>
2017-08-03 23:30:39 +0200

    work with autolayout

---
+++ Myswoosh/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  Myswoosh
+//
+//  Created by Zaidi Abdelkoddous on 03.08.17.
+//  Copyright © 2017 ZAK. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
